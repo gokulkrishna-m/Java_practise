60f9d5464ebd9e2b04ba1d88665d8227e723f2bc
gokulkrishna-m <gokulkrishnamanikumar@gmail.com>
2021-04-01 18:12:57 +0530

    commit

--- src/TestArray.java
+++ src/TestArray.java
@@ -9,6 +9,29 @@ public class TestArray {
         System.out.println("Count :"+count++);
         System.out.println("Count :"+count++);
         System.out.println("Count :"+count++);
+
+        String name = "Gokul"+System.lineSeparator()+"Krishna"+System.lineSeparator()+"GokulKrishna";
+
+        String[] na = name.split("[\r\n]+");
+        //System.out.println(na.length);
+        String n1 = name.split("[\r\n]+")[0];
+        String n2 = name.split("[\r\n]+")[1];
+        String n3 = name.split("[\r\n]+")[2];
+
+        System.out.println("N1 :"+n1+" ||N2 :"+n2+" ||N3 :"+n3);
+
+        double d2 = 0;
+        System.out.println("sdf :"+(d2 == 0.0));
+
+        String d = "0";
+        String d1 = String.valueOf(Double.parseDouble(d));
+
+        System.out.println("fsdf :"+d1);
+        if(d1.trim().equals("0.0")){
+            System.out.println("Gokul :"+d1.trim());
+        }else{
+            System.out.println("Krishna :"+d1.trim());
+        }
     }
 
     public static void sample(String... args){
