038f14c323d0c97d99dbf03ce565cde185744989
gokulkrishna-m <gokulkrishnamanikumar@gmail.com>
2021-05-24 11:51:12 +0530

    commit

--- src/IntellectTestNumber.java
+++ src/IntellectTestNumber.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class IntellectTestNumber {
     public static void main(String[] args) {
         int n = 1;
@@ -32,5 +35,35 @@ public class IntellectTestNumber {
         }else{
             System.out.println("Success");
         }
+
+        int a1 = -10;
+        int a2 = 10;
+
+        System.out.println("a1 :"+Math.abs(a1));
+        System.out.println("a2 :"+Math.abs(a2));
+
+        System.out.println("abs1 :"+abs1(a1));
+        System.out.println("abs2 :"+abs1(a2));
+
+        int arr[] = {3,2,4,1};
+
+        for (int g:arr) {
+            System.out.println("ARR :"+g);
+        }
+
+        Arrays.sort(arr);
+        for (int g:arr) {
+            System.out.println("Sorted :"+g);
+        }
+        //System.out.println("Sorted :"+ arr);
+
+    }
+
+    public static int abs1(int a) {
+        //return (a < 0) ? -a : a;
+        if(a < 0){
+            return -a;
+        }
+        return a;
     }
 }
